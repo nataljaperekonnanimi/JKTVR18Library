e5715e919d05c71ac084c5d634ca37b0b0daf2d6
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2019-09-10 13:35:40 +0300

    Задали null id сущностям

--- src/classes/App.java
+++ src/classes/App.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class App {
     public void run(){
         Book book = new Book();
-        book.setId(1L);
+        
         book.setTitle("Voina i mir");
         book.setAuthor("L.Tolstoy");
         book.setYear(2010);
@@ -28,18 +28,18 @@ public class App {
         System.out.println("Вывод метода toString(): ");
         System.out.println(book.toString());
         
-        Book book2 = new Book(2L, "Otsi i deti", "I.Turgenev", 2011);
+        Book book2 = new Book(null, "Otsi i deti", "I.Turgenev", 2011);
         System.out.println(book2.toString());
         
         book.setYear(2012);
         System.out.println(book.toString());
         
-        Reader reader = new Reader(1L, "Ivan", "Ivanov", 10, 10, 2000);
+        Reader reader = new Reader(null, "Ivan", "Ivanov", 10, 10, 2000);
         System.out.println(reader.toString());
                 
         System.out.println("Выдача книги:");
         History history = new History();
-        history.setId(1L);
+        
         history.setBook(book);
         history.setReader(reader);
         history.setTakeOnDate(new Date());
